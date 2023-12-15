09d677ce78ba6c422f3e9fa1955e71bd79bf400b
Glenda Jocelyn <glenskim24@gmail.com>
2023-12-15 23:39:10 +0700

    login regis starter done

---
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    ADMINN
+</div>
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Livewire\Admin\AdminDashboardComponent;
+use App\Livewire\CartComponent;
+use App\Livewire\CheckoutComponent;
+use App\Livewire\HomeComponent;
+use App\Livewire\ShopComponent;
+use App\Livewire\User\UserDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", HomeComponent::class)->name("home.index");
+
+Route::get("/shop", ShopComponent::class)->name("shop");
+
+Route::get("/cart", CartComponent::class)->name("shop.cart");
+
+Route::get("/checkout", CheckoutComponent::class)->name("shop.checkout");
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+Route::middleware(['auth','authadmin'])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
