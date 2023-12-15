7effe7a2cee82e17ee9ad4814b3108da311ddacb
Glenda Jocelyn <glenskim24@gmail.com>
2023-12-16 02:52:22 +0700

    cart done

--- app/Livewire/CartComponent.php
+++ app/Livewire/CartComponent.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId, $qty);
+        // $this->emitTo('cart-icon-component','refreshComponent');
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId, $qty);
+        // $this->emitTo('cart-icon-component','refreshComponent');
+    }
+
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        // $this->emitTo('cart-icon-component','refreshComponent');
+        session()->flash("success_message","Item has been removed!");
+    }
+
+    public function clearAll()
+    {
+        Cart::destroy();   
+        // $this->emitTo('cart-icon-component','refreshComponent');     
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
