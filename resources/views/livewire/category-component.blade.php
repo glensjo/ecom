aa791dd3ebc49b0d3c92e46cd9b3944c41c5e8a2
Glenda Jocelyn <glenskim24@gmail.com>
2023-12-22 16:11:04 +0700

    revised

--- resources/views/livewire/category-component.blade.php
+++ resources/views/livewire/category-component.blade.php
@@ -71,7 +71,6 @@
                                             <div class="product-img product-img-zoom">
                                                 <a href="{{ route('product.details',['slug'=>$product->slug]) }}">
                                                     <img class="default-img" src="{{ asset('assets/imgs/products') }}/{{$product->image}}" alt="{{$product->name}}">
-                                                    <img class="hover-img" src="{{ asset('assets/imgs/shop/product-') }}{{$product->id}}-2.jpg" alt="{{$product->name}}">
                                                 </a>
                                             </div>
                                             <div class="product-action-1">
